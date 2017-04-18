0e8e13b561a5e70dedf789dd7b7eef01fc37d665
tanaxiusi <373923276@qq.com>
2017-04-18 14:44:52 +0800

    修复上个提交Qt4编译错误。

--- Client/Gui/GuiMain.cpp
+++ Client/Gui/GuiMain.cpp
@@ -28,7 +28,10 @@ bool showGuideReturnCanContinue()
 
 int GuiMain(int argc, char *argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
 	QApplication app(argc, argv);
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
